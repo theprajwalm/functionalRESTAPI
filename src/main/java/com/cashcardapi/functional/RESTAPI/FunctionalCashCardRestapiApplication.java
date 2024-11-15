5275258f4679c454bd0174d4971fc4a33d29c721
theprajwalm <prajwalmishra.cys@gmail.com>
2024-11-15 14:06:07 +0100

    first commit

---
+++ src/main/java/com/cashcardapi/functional/RESTAPI/FunctionalCashCardRestapiApplication.java
@@ -0,0 +1,13 @@
+package com.cashcardapi.functional.RESTAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FunctionalCashCardRestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FunctionalCashCardRestapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cashcardapi/functional/RESTAPI/FunctionalCashCardRestapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cashcardapi.functional.RESTAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FunctionalCashCardRestapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
